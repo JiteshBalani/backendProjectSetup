bba59f6ad18779c30f7b367a4e791ceeb5899032
JiteshBalani <balani.jitesh911@gmail.com>
2024-07-13 22:05:39 +0530

    express cors cookieParser installed and utils created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import dotenv from "dotenv"
-import mongoose from "mongoose";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
     path: './env'
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port: ${process.env.PORT}`);
+    }) 
+    app.on("CONNECTION TO SERVER FAILED...!", (error) => {
+        console.log("FAILED TO CONNECT TO SERVER.");
+        throw error; 
+    })
+})
+.catch((error) => {
+    console.log("MONGO-DB CONNECTION FAILED...!" , error)
+    process.exit(1)
+})
 
 
 
